7482f52e2dd5bc2d3a5c756bbb4c04e806fdcd03
eulucasm <lucaspoletis@gmail.com>
2021-05-17 15:10:49 -0300

    Continuação curso

--- app.js
+++ app.js
@@ -2,6 +2,33 @@
 var app = require('./config/server');
 
 /*parametrizar a porta de escuta */
-app.listen(80, function(){
+var server = app.listen(80, function(){
 	console.log('servidor online');
+})
+
+var io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+/*criar a conexão por websocket */
+io.on('connection', function(socket){
+	console.log('usuario conectou');
+
+	socket.on('disconnect', function(){
+		console.log('usuario desconectou');
+	});
+
+	socket.on('msgParaServidor', function(data){
+		socket.emit(
+			'msgParacliente',
+			{apelido : data.apelido, mensagem : data.mensagem}
+		);//aqui a msg enviada aparecera para quem enviou
+
+		socket.broadcast.emit(
+			'msgParacliente',
+			{apelido : data.apelido, mensagem : data.mensagem}
+		);//aqui a msg enviada aparecera para os demais participantes do chat
+	});
 });
+/* Basicamente, a função 'emit' faz um pedido parar executar alguma ação
+e o 'on' fica ouvindo os pedidos de execução*/
--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -12,5 +12,10 @@ module.exports.iniciaChat = function(application, req, res){
 		return;
 	}
 
-	res.render("chat");
+	application.get('io').emit(
+		'msgParaCliente',
+		{apelido : dadosForm.apelido, mensagem: 'Acabou de entrar no chat'}
+	)
+
+	res.render("chat", {dadosForm : dadosForm});
 }
